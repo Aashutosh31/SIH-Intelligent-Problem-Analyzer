34af3ac81e27cd7bddf99399a4602baba74a84cb
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2026-08-24 20:31:07 +0530

    feat(team): define team profile data model

---
+++ frontend/src/types/team.js
@@ -0,0 +1,19 @@
+export const DEFAULT_TEAM_PROFILE = {
+  name: "My SIH Team",
+
+  members: [
+    {
+      id: "member-1",
+      name: "",
+      role: "",
+      skills: [],
+    },
+  ],
+
+  preferences: {
+    softwareOnly: false,
+    hardwareComfort: 5,
+    aiMlComfort: 5,
+    willingnessToLearn: 5,
+  },
+};
